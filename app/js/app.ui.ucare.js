592e07884d53dee77f41171d50ba8d33e1239c8f
evaletolab <evaleto@gmail.com>
2015-04-09 10:58:41 +0200

    angular 1.3 migration plus uploadcare widget

---
+++ app/js/app.ui.ucare.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angular-uploadcare.directive:Uploadcare
+ * @description Provides a directive for the Uploadcare widget.
+ * # Uploadcare
+ * # see https://uploadcare.com/cookbook/validation/
+ */
+angular.module('ngUploadcare', ['app.config'])
+  .directive('uploadcareWidget', uploadcareWidget);
+
+uploadcareWidget.$inject=['config', '$parse','$timeout','$log'];
+function uploadcareWidget(config, $parse, $timeout, $log) {
+  return {
+    restrict: 'E',
+    replace: true,
+    require: 'ngModel',
+    template: '<input type="hidden" role="ng-uploadcare-uploader" />',
+    scope: {
+      onWidgetReady: '&',
+      onUploadComplete: '&',
+      onUploadError: '&',
+      onChange: '&',
+    },
+    link:function($scope, $element, $attrs) {
+      if(!uploadcare) {
+        $log.error('Uploadcare script has not been loaded!.');
+        return;
+      }
+      // uploadcare.openDialog(null, {
+      //     publicKey: config.uploadcare,
+      // });      
+      $scope.widget = uploadcare.Widget($element);
+      $scope.widget.validators.push(function(fileInfo) {
+        if (fileInfo.size !== null && fileInfo.size > 150 * 1024) {
+          $scope.onUploadError({error: "La taille maximum d'une image est limitée à 150kb"});
+          throw new Error("fileMaximumSize");
+        }
+      });      
+      // $scope.onWidgetReady({widget: $scope.widget});
+      $scope.widget.onUploadComplete(function(info) {
+        $scope.onUploadComplete({info: info});
+      });
+      $scope.widget.onChange(function(file) {
+        if(file){
+          file.done(function(fileInfo) {
+            // $timeout(function() {
+            //   $parse($attrs.ngModel).assign($scope.$parent, '//ucarecdn.com/'+fileInfo.uuid+'/');
+            // },200)
+            $scope.$apply(function () {
+              $parse($attrs.ngModel).assign($scope.$parent, '//ucarecdn.com/'+fileInfo.uuid+'/');
+            });
+          }).fail(function(error, fileInfo) {
+            $log.error('Uploadcare upload error',error,fileInfo);
+            $scope.onUploadError({error: error});
+          });        
+
+        }
+
+        // add data binding for hidden inputs
+        $scope.onChange({file: file});
+      })
+    }
+  };
+};
